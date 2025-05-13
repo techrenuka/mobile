4d4ea77ba8049e710afc7290127b6e42e2478cc1
shaileshmakwana099 <shaileshmakwana099@gmail.com>
2025-05-13 11:32:10 +0530

    fix error 2

--- src/components/Home/BestSeller/SingleItem.tsx
+++ src/components/Home/BestSeller/SingleItem.tsx
@@ -24,6 +24,7 @@ const SingleItem = ({ item }: { item: Product }) => {
     dispatch(
       addItemToCart({
         ...item,
+        discountedPrice: item.price,
         quantity: 1,
       })
     );
@@ -34,6 +35,7 @@ const SingleItem = ({ item }: { item: Product }) => {
       addItemToWishlist({
         ...item,
         status: "available",
+        discountedPrice: item.price,
         quantity: 1,
       })
     );
@@ -44,50 +46,15 @@ const SingleItem = ({ item }: { item: Product }) => {
       <div className="relative overflow-hidden rounded-lg bg-[#F6F7FB] min-h-[403px]">
         <div className="text-center px-4 py-7.5">
           <div className="flex items-center justify-center gap-2.5 mb-2">
-            <div className="flex items-center gap-1">
-              <Image
-                src="/images/icons/icon-star.svg"
-                alt="star icon"
-                width={14}
-                height={14}
-              />
-              <Image
-                src="/images/icons/icon-star.svg"
-                alt="star icon"
-                width={14}
-                height={14}
-              />
-              <Image
-                src="/images/icons/icon-star.svg"
-                alt="star icon"
-                width={14}
-                height={14}
-              />
-              <Image
-                src="/images/icons/icon-star.svg"
-                alt="star icon"
-                width={14}
-                height={14}
-              />
-              <Image
-                src="/images/icons/icon-star.svg"
-                alt="star icon"
-                width={14}
-                height={14}
-              />
-            </div>
-
-            <p className="text-custom-sm">({item.reviews})</p>
-          </div>
 
           <h3 className="font-medium text-dark ease-out duration-200 hover:text-blue mb-1.5">
             <Link href="/shop-details"> {item.title} </Link>
           </h3>
 
           <span className="flex items-center justify-center gap-2 font-medium text-lg">
-            <span className="text-dark">${item.discountedPrice}</span>
-            <span className="text-dark-4 line-through">${item.price}</span>
+            <span className="text-dark">${item.price}</span>
           </span>
+          </div>
         </div>
 
         <div className="flex justify-center items-center">
